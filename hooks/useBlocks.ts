741113accb381643fe3e102a5f8502cc3e63b8d6
ProMarketnet <scandi1803@gmail.com>
2025-07-30 19:29:23 -0500

    Update useBlocks.ts

--- hooks/useBlocks.ts
+++ hooks/useBlocks.ts
@@ -1,1 +1,10 @@
+import { useContext } from 'react';
+import { BlockContext } from '../context/BlockContext';
 
+export const useBlocks = () => {
+  const context = useContext(BlockContext);
+  if (context === undefined) {
+    throw new Error('useBlocks must be used within a BlockProvider');
+  }
+  return context;
+};
