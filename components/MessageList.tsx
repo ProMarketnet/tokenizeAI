c61dce478295e91497f02ca841f28cb154097ccc
ProMarketnet <scandi1803@gmail.com>
2025-07-30 19:39:31 -0500

    Update MessageList.tsx

--- components/MessageList.tsx
+++ components/MessageList.tsx
@@ -1,1 +1,22 @@
+import React from 'react';
+import { Message } from '../types';
+import MessageBubble from './MessageBubble';
+import TypingIndicator from './TypingIndicator';
 
+interface MessageListProps {
+  messages: Message[];
+  isTyping: boolean;
+}
+
+const MessageList: React.FC<MessageListProps> = ({ messages, isTyping }) => {
+  return (
+    <div className="p-4 space-y-2 min-h-full">
+      {messages.map((message) => (
+        <MessageBubble key={message.id} message={message} />
+      ))}
+      {isTyping && <TypingIndicator />}
+    </div>
+  );
+};
+
+export default MessageList;
