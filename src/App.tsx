27b883d4aa78937c1dc5358424fe412c9a990a97
ProMarketnet <scandi1803@gmail.com>
2025-07-30 19:31:26 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,18 @@
+import React from 'react';
+import ChatInterface from './components/ChatInterface';
+import { ChatProvider } from './context/ChatContext';
+import { BlockProvider } from './context/BlockContext';
 
+function App() {
+  return (
+    <div className="App h-screen bg-background">
+      <ChatProvider>
+        <BlockProvider>
+          <ChatInterface />
+        </BlockProvider>
+      </ChatProvider>
+    </div>
+  );
+}
+
+export default App;
